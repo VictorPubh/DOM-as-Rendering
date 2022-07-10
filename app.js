84b6b8953c2f947ce9b9486b56453e6273ff08de
João Victor Sant'ana <viitorsant@gmail.com>
2022-07-09 22:13:57 -0300

    fix listen port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
 
+const PORT = 3000;
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
@@ -13,7 +14,7 @@ app.use('/', routes);
 app.use(express.static('assets'));
 
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('Route not defined')));
-app.listen(3000, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log('Available on: http://localhost:3000')
 })
 
